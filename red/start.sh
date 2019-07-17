994b67520457c541f7b66b3027fd9f89e7831eaa
Roman Bosak <158157+bosy@users.noreply.github.com>
2019-07-17 23:03:01 +0200

    logs folder created and log timestamp introduced

--- red/start.sh
+++ red/start.sh
@@ -4,4 +4,6 @@ sudo sysctl -w vm.nr_hugepages=128
 
 ./set_power_cap.txt
 
-~/xmr-stak/build/bin/xmr-stak --noCPU --h-print-time 60 --log logs/log.txt --config xmr-stak/config.txt --poolconf xmr-stak/pools.txt --cpu xmr-stak/cpu.txt --amd xmr-stak/amd.txt
+mkdir -p logs
+
+~/xmr-stak/build/bin/xmr-stak --noCPU --h-print-time 60 --log logs/log-$(date -d "today" +"%Y%m%d-%H%M%S").txt --config xmr-stak/config.txt --poolconf xmr-stak/pools.txt --cpu xmr-stak/cpu.txt --amd xmr-stak/amd.txt
