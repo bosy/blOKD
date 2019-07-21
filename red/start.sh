6ad1d201d11414ddf87258846d138123c826ced4
Roman Bosak <158157+bosy@users.noreply.github.com>
2019-07-21 12:53:38 +0200

    output from set_power_cap included into log file

--- red/start.sh
+++ red/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+LOG_FILE="logs/log-$(date -d 'today' +'%Y%m%d-%H%M%S').txt"
+
 sudo sysctl -w vm.nr_hugepages=128
 
-./set_power_cap.txt
+./set_power_cap.txt 2>&1 | tee -a $LOG_FILE
 
-mkdir -p logs
+mkdir -p logs 2>&1 | tee -a $LOG_FILE
 
-~/xmr-stak/build/bin/xmr-stak --noCPU --h-print-time 60 --log logs/log-$(date -d "today" +"%Y%m%d-%H%M%S").txt --config xmr-stak/config.txt --poolconf xmr-stak/pools.txt --cpu xmr-stak/cpu.txt --amd xmr-stak/amd.txt
+~/xmr-stak/build/bin/xmr-stak --noCPU --h-print-time 60 --log $LOG_FILE --config xmr-stak/config.txt --poolconf xmr-stak/pools.txt --cpu xmr-stak/cpu.txt --amd xmr-stak/amd.txt
